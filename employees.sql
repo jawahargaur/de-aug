c6fe722b1e6f539fa2d1bf4af047c7201b8d90bb
Jawahar Gaur <107173050+jawahargaur@users.noreply.github.com>
2024-11-30 19:33:04 +0530

    Update employees.sql

--- employees.sql
+++ employees.sql
@@ -7,8 +7,5 @@ DepartmentID INT,
 Salary DECIMAL(15, 2),
 Address NVARCHAR(255),
 PhoneNumber NVARCHAR(20),
-Email NVARCHAR(100) UNIQUE,
-IsActive BIT DEFAULT 1,
-CONSTRAINT FK_Department FOREIGN KEY (DepartmentID) REFERENCES
-Departments(DepartmentID)
+Email NVARCHAR(100) UNIQUE
 );
